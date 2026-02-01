b3aee0c98139a8efbe136df18d6a531c084a22ae
marcosvcg <marcos.vdcg@gmail.com>
2026-02-01 16:39:38 -0300

    FEAT: adiciona rotas dos recursos

---
+++ api/resources.py
@@ -0,0 +1,8 @@
+''' Recursos fornecidos hoje (01/02/2026) pela SWAPI '''
+
+PEOPLE = 'people'
+FILMS = 'films'
+STARSHIPS = 'starships'
+VEHICLES = 'vehicles'
+SPECIES = 'species'
+PLANETS = 'planets'
--- api/router.py
+++ api/router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from api.utils import query
+from api import resources
 
 router = APIRouter()
 API_URL = "https://swapi.dev/api/"
 
-def _fetch(resource, id):
+def _fetch(resource: resources, id):
     result = query("{0}/{1}/{2}".format(
         API_URL,
         resource,
@@ -12,7 +13,37 @@ def _fetch(resource, id):
     )
     return result
 
+@router.get("/{resource}")
+async def get_all_resource_data(resource):
+    result = query("{0}/{1}/".format(API_URL, resource))
+    return result.json()
+
 @router.get("/people/{id}")
 async def get_people(id):
-    result = _fetch("people", id)
+    result = _fetch(resources.PEOPLE, id)
+    return result.json()
+
+@router.get("/film/{id}")
+async def get_film(id):
+    result = _fetch(resources.FILMS, id)
+    return result.json()
+
+@router.get("/starship/{id}")
+async def get_starship(id):
+    result = _fetch(resources.STARSHIPS, id)
+    return result.json()
+
+@router.get("/vehicle/{id}")
+async def get_vehicle(id):
+    result = _fetch(resources.VEHICLES, id)
+    return result.json()
+
+@router.get("/specie/{id}")
+async def get_species(id):
+    result = _fetch(resources.SPECIES, id)
+    return result.json()
+
+@router.get("/planet/{id}")
+async def get_planet(id):
+    result = _fetch(resources.PLANETS, id)
     return result.json()
