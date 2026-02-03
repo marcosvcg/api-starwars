bd0dcfc147ac7561ea53ac5a405440a1f4f379c4
marcosvcg <marcos.vdcg@gmail.com>
2026-02-03 09:42:26 -0300

    REFACTOR: melhora a tipagem dos recursos

--- api/resources.py
+++ api/resources.py
@@ -1,8 +1,11 @@
-''' Recursos fornecidos hoje (01/02/2026) pela SWAPI '''
+''' Recursos fornecidos hoje (03/02/2026) pela SWAPI '''
 
-PEOPLE = 'people'
-FILMS = 'films'
-STARSHIPS = 'starships'
-VEHICLES = 'vehicles'
-SPECIES = 'species'
-PLANETS = 'planets'
+from enum import StrEnum
+
+class Resources(StrEnum):
+    PEOPLE = "people"
+    FILMS = "films"
+    STARSHIPS = "starships"
+    VEHICLES = "vehicles"
+    SPECIES = "species"
+    PLANETS = "planets"
--- api/router.py
+++ api/router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from api.utils import query, build_params
-from api import resources
+from api.resources import Resources
 import math
 
 router = APIRouter()
 API_URL = "https://swapi.dev/api/"
 
-def _fetch(resource: resources, id):
+def _fetch(resource: Resources, id):
     result = query("{0}/{1}/{2}".format(
         API_URL,
         resource,
@@ -16,7 +16,7 @@ def _fetch(resource: resources, id):
 
 @router.get("/{resource}")
 async def get_all_resource_data(
-    resource,
+    resource: Resources,
     page: int = 1,
     search: str | None = None
 ):
@@ -33,30 +33,30 @@ async def get_all_resource_data(
 
 @router.get("/person/{id}")
 async def get_person(id):
-    result = _fetch(resources.PEOPLE, id)
+    result = _fetch(Resources.PEOPLE, id)
     return result.json()
 
 @router.get("/film/{id}")
 async def get_film(id):
-    result = _fetch(resources.FILMS, id)
+    result = _fetch(Resources.FILMS, id)
     return result.json()
 
 @router.get("/starship/{id}")
 async def get_starship(id):
-    result = _fetch(resources.STARSHIPS, id)
+    result = _fetch(Resources.STARSHIPS, id)
     return result.json()
 
 @router.get("/vehicle/{id}")
 async def get_vehicle(id):
-    result = _fetch(resources.VEHICLES, id)
+    result = _fetch(Resources.VEHICLES, id)
     return result.json()
 
 @router.get("/specie/{id}")
 async def get_species(id):
-    result = _fetch(resources.SPECIES, id)
+    result = _fetch(Resources.SPECIES, id)
     return result.json()
 
 @router.get("/planet/{id}")
 async def get_planet(id):
-    result = _fetch(resources.PLANETS, id)
+    result = _fetch(Resources.PLANETS, id)
     return result.json()
