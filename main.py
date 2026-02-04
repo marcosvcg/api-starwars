af90fb59f68fce6115230703132f01bb2cd590f0
marcosvcg <marcos.vdcg@gmail.com>
2026-02-04 08:46:57 -0300

    FEAT: implementa configuracoes de CORS

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.router import router
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ["https://www.powerofdata.ai", "http://localhost:5173"],
+    allow_credentials= True,
+    allow_methods= ["*"],
+    allow_headers= ["*"],
+)
 
 @app.get("/api/healthcheck")
 def healthcheck():
